c906af25a89888969665430f147f634e368452bc
Bilal-Ali-wdev <bilalwdev26@gmail.com>
2026-08-13 13:01:24 +0500

    add jest test case

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from "express"
-export const app = express()
+const express = require("express")
+ const app = express()
 app.use(express.json())
 
 app.get("/", (req, res) => {
@@ -9,3 +9,5 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
+
+module.exports = { app }
